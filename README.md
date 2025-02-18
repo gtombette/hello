69fbe05c080ba3803d329a018a4255fb2860b8b1
gongonchu <gongonchu@gmail.com>
2025-02-18 10:58:08 +0100

    Creation of the project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hello Application
+This is a example application to use GCP Cloud Build
---
+++ src/main/java/com/worldcompany/hello/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.worldcompany.hello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String welcomeMessage(){
+        return "Hello! How are you?";
+    }
+
+}
