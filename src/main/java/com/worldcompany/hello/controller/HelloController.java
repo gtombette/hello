a578a8fa8747d0bd23176f3637da761d9eefc499
Gontran TOMBETTE <gontran.tombette@devoteam.com>
2025-04-01 11:22:13 +0200

    Update HelloController.java

--- src/main/java/com/worldcompany/hello/controller/HelloController.java
+++ src/main/java/com/worldcompany/hello/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String welcomeMessage(){
-        return "Hello! How are you?";
+        return "Hello! How are you guys?";
     }
 
 }
